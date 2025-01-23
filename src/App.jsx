8271140a848b0a28c0d4f836d55862709db0ebea
julio-cesar96 <julio_cbs96@hotmail.com>
2025-01-23 19:54:28 -0300

    aula do dia 21/01

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import { useEffect } from "react";
+import { Menu } from "./components/Menu/Menu"
 import { Saudacao } from "./components/Saudacao/Saudacao"
 import ListaDePost from "./ListaDePost"
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom"
 
 function App() {
-   
+   const { userId } = useParams();
+   useEffect(() => {
+      console.log('Recebendo userId:', userId);
+      return () => {
+        console.log('userId mudando:', userId);
+      }
+   }, [userId]);
   return (
     <>
-      <Saudacao name="Diego" />
-      <ListaDePost />
+      <BrowserRouter>
+      <Menu />
+        <Routes>
+          <Route exact path="/" element={<Saudacao name="Julio" />}/>
+          <Route exact path="/post" element={<ListaDePost />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
