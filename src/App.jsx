4b60d4256ff230c757793f2b8684d19ac05785d3
julio-cesar96 <julio_cbs96@hotmail.com>
2025-01-28 19:11:03 -0300

    aula do dia 26/01

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,14 @@
-import { useEffect } from "react";
-import { Menu } from "./components/Menu/Menu"
-import { Saudacao } from "./components/Saudacao/Saudacao"
-import ListaDePost from "./ListaDePost"
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom"
-
 function App() {
-   const { userId } = useParams();
-   useEffect(() => {
-      console.log('Recebendo userId:', userId);
-      return () => {
-        console.log('userId mudando:', userId);
-      }
-   }, [userId]);
+   const handleClickButton = (event) => {
+    console.log("Fui clicado", event);
+   }
+
+   const handleInput = (event) => {
+   }
   return (
     <>
-      <BrowserRouter>
-      <Menu />
-        <Routes>
-          <Route exact path="/" element={<Saudacao name="Julio" />}/>
-          <Route exact path="/post" element={<ListaDePost />} />
-        </Routes>
-      </BrowserRouter>
+      <button type="button" onClick={handleClickButton}>Clique em mim </button>
+      <input type="text" onKeyDown={handleInput} />
     </>
   )
 }
